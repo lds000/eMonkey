4ef50bb236d1a990725d5f7cd8e665459853aa1c
lds000 <52395701+lds000@users.noreply.github.com>
2025-02-12 15:50:30 -0700

    Add handke

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,7 +12,12 @@ namespace eMonkey
             InitializeComponent();
             _viewModel = new PatientVisitViewModel();
             DataContext = _viewModel;
-            _viewModel.LoadPatientVisits(new IntPtr(66746)); // Example handle
+
+            //get handle from a window with Title starting with "eCW"
+            var handle = WindowHelper.GetWindowHandle("eCW");
+
+
+            _viewModel.LoadPatientVisits(new IntPtr(handle)); // Example handle
         }
     }
 }
